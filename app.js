01198ffc1d1d61b48cc8f48c7e05a5c19ba2655d
Patryk Byszek <byszek.patryk@gmail.com>
2021-03-22 18:48:02 +0100

    Modified the js code, also added keydown event

--- app.js
+++ app.js
@@ -18,16 +18,88 @@ const percentage = () =>{
       document.getElementById("output").innerHTML = value;
 }   
 
-const turnOnLight = () =>{
-  let light = document.querySelector('.statusLight');
-  light.classList.add('lighton')
-}
 
 function forDisplay(value) {
       removeZero()
       document.getElementById("output").innerHTML += value;
  
     }
+
+const keyboardPress = (event) => {
+    removeZero();
+    if(event.keyCode === 49 || event.keyCode === 97){
+        document.getElementById("output").innerHTML += 1
+    }
+    if(event.keyCode === 50 || event.keyCode === 98){
+        document.getElementById("output").innerHTML += 2
+    }
+    if(event.keyCode === 51 || event.keyCode === 99){
+        document.getElementById("output").innerHTML += 3
+    }
+    if(event.keyCode === 52 || event.keyCode === 100){
+        document.getElementById("output").innerHTML += 4
+    }
+    if(event.keyCode === 53 || event.keyCode === 101){
+        document.getElementById("output").innerHTML += 5
+    }
+    if(event.keyCode === 54 || event.keyCode === 102){
+        document.getElementById("output").innerHTML += 6
+    }
+    if(event.keyCode === 55 || event.keyCode === 103){
+        document.getElementById("output").innerHTML += 7
+    }
+    if(event.keyCode === 57 || event.keyCode === 105){
+        document.getElementById("output").innerHTML += 9
+    }
+    // if(event.keyCode === 88){
+    //     forDisplay('*')
+    // }
+    // if(event.shiftKey && event.keyCode === 56){    
+    //     console.log('*')
+    //     forDisplay('*')
+    // }
+    
+    if (event.shiftKey) {
+        if (event.keyCode == 56) {
+        forDisplay('*')
+        } else if (event.keyCode == 187) {
+        forDisplay("+");
+        }
+    }
+     
+    
+    // if (event.keyCode === 56 || event.keyCode === 104) {
+    //     if (event.shiftKey) document.getElementById("output").innerHTML += "*";
+    //     else{
+    //         document.getElementById("output").innerHTML += 8
+    //     }
+    // }
+
+
+
+    if(event.keyCode === 49 || event.keyCode === 107){
+        forDisplay('+')
+    }
+    if(event.keyCode === 189 || event.keyCode === 109){
+        forDisplay('-')
+    }
+    if(event.keyCode === 190 || event.keyCode === 110){
+        forDisplay('.')
+    }
+    if(event.keyCode === 191 || event.keyCode === 111){
+        forDisplay('/')
+    }
+    if(event.keyCode === 16 && event.keyCode === 53){
+        forDisplay('%')
+    }
+    if(event.keyCode === 27 || event.keyCode === 67){
+        clearScreen()
+    }
+    if(event.keyCode === 13 || event.keyCode === 187){
+        solve()
+    }
+}
+
 
 const solve = () =>{
     removeZero()
@@ -37,13 +109,19 @@ const solve = () =>{
 }
 
 
-
 const moveInput = () =>{
    let input = document.getElementById('output').innerHTML
 document.querySelector('.previousInput').innerHTML += input;
 value = document.getElementById('output').innerHTML = ''
 }
 
+const turnOnLight = () =>{
+  let light = document.querySelector('.statusLight');
+  light.classList.add('lighton')
+}
+
+
+document.addEventListener('keydown', keyboardPress)
 
 
 
@@ -51,9 +129,11 @@ value = document.getElementById('output').innerHTML = ''
 
 
 
-
-
-
+document.addEventListener('keydown', ()=>{
+ if(event.shiftKey && event.keyCode){    
+        forDisplay('*')
+    }
+})
 
 
 
